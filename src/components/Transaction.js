475a0dedb9a1b32d1d076740cbb8db4e736f2b99
Rebecca Bartl <rebecca.bartl@gmail.com>
2020-05-19 15:22:51 -0400

    add transaction into list

---
+++ src/components/Transaction.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Transaction = (props) => {
+    return (
+        <li className="minus">
+            {props.transaction.text} <span>-$400</span><button className="delete-btn">x</button>
+        </li>
+    )
+}
